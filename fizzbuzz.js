d9de757b385d5d8ea7724a82712c6b7aa4386140
William Jeffries <wm.jeffries@gmail.com>
2020-10-18 17:17:36 +0900

    Implement fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,7 @@
-const fizzbuzz = () => {
-  return
+const fizzbuzz = (n) => {
+  const fizz = n % 3 === 0 ? 'fizz' : ''
+  const buzz = n % 5 === 0 ? 'buzz' : ''
+  return `${fizz}${buzz}` || `${n}`
 }
 
 export default fizzbuzz
--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -1,5 +1,35 @@
-import fizzbuzz from "./fizzbuzz";
+import fizzbuzz from './fizzbuzz'
 
-test('returns 1 for 1', () => {
-  expect(fizzbuzz(1)).toEqual(1)
+describe("default behavior", () => {
+  it('returns 1 for 1', () => {
+    expect(fizzbuzz(1)).toEqual('1')
+  })
+
+  it('returns 2 for 2', () => {
+    expect(fizzbuzz(2)).toEqual('2')
+  })
+})
+
+describe("numbers divisible by 3", () => {
+  it('returns fizz for 3', () => {
+    expect(fizzbuzz(3)).toEqual('fizz')
+  })
+
+  it('returns fizz for 6', () => {
+    expect(fizzbuzz(6)).toEqual('fizz')
+  })
+})
+
+describe('numbers divisible by 5', () => {
+  it('returns buzz for 5', () => {
+    expect(fizzbuzz(5)).toEqual('buzz')
+  })
+
+  it('returns buzz for 10', () => {
+    expect(fizzbuzz(10)).toEqual('buzz')
+  })
+})
+
+describe('numbers divisible by both 3 and 5', () => {
+  expect(fizzbuzz(15)).toEqual('fizzbuzz')
 })
