660fc83d94d0236c47b25b2843c5dc693ca68f95
William Jeffries <wm.jeffries@gmail.com>
2020-10-18 17:17:08 +0900

    Set up test config

--- jest.config.js
+++ jest.config.js
@@ -141,10 +141,11 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    // "**/__tests__/**/*.[jt]s?(x)",
+    // "**/?(*.)+(spec|test).[tj]s?(x)"
+    "**/*.spec.js"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
